8d51b503d99378325ee22a35a9d857d8d08f3f19
Raushan Raj <raushanraj1205@gmail.com>
2024-11-27 20:43:42 +0530

    api added to add like

--- app/api/post/likeByPostId/route.tsx
+++ app/api/post/likeByPostId/route.tsx
@@ -2,7 +2,34 @@ import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 export async function POST(request: NextRequest) {
-  const body = await request.json();
-  console.log(body);
-  return NextResponse.json({ message: body });
+  const { postId, userId } = await request.json();
+  const existingLike = await prisma.postLike.findUnique({
+    where: {
+      userId_postId: { userId, postId }, // Composite unique constraint
+    },
+  });
+
+  if (existingLike) {
+    return NextResponse.json({
+      message: "Already There",
+    });
+  }
+  await prisma.postLike.create({
+    data: {
+      userId,
+      postId,
+    },
+  });
+  const getpost = await prisma.post.update({
+    where: { id: postId },
+    data: {
+      likedBy: {
+        connect: { id: userId }, // Connect the user
+      },
+      likecount: {
+        increment: 1,
+      },
+    },
+  });
+  return NextResponse.json(getpost);
 }
