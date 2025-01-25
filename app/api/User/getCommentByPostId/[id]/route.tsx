eb6f11d47e8a75486ee09470e6742bf707db6a76
Raushan Raj <raushanraj1205@gmail.com>
2025-01-25 16:04:33 +0530

    Update route.tsx of getCommentByPostId

--- app/api/User/getCommentByPostId/[id]/route.tsx
+++ app/api/User/getCommentByPostId/[id]/route.tsx
@@ -6,7 +6,7 @@ export async function GET(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
-  const { id } = await params;
+  const { id } = params;
   const getsPosts = await prisma.comment.findMany({
     where: {
       postId: id,
